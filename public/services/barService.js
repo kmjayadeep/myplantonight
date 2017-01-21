2d29ac7375eb7a27415c0d4245d3c9a028ca210d
Jayadeep KM <kmjayadeep@gmail.com>
2017-01-21 10:54:47 +0530

    initial commit

---
+++ public/services/barService.js
@@ -0,0 +1,19 @@
+let $ = requrie('jquery')
+let promise = requrie('es6-promise')
+let resourceUrl = "http://localhost:3000/api/bar";
+
+module.exports = {
+	search: (query)=>{
+		let Promise = promise.Promise
+		return new Promise((resolve,reject)=>{
+			$.ajax({
+				url:resourceUrl+'/search/'+query,
+				method:'GET',
+				dataType:'json',
+				contentType:'application/json',
+				success:resolve,
+				error:reject
+			})
+		})
+	}
+}
